a989af92d357f7121eabfba312f9d291f68a29d1
Kevin Longe <kvlonge@gmail.com>
2026-07-12 06:19:27 +0100

    style(map): use cyan function names

--- src/strata/mapping/core/constants.py
+++ src/strata/mapping/core/constants.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 ANSI_RESET: str = "\x1b[0m"
 ANSI_DIM: str = "\x1b[2m"
-ANSI_FUNCTION: str = "\x1b[38;5;208m"
+ANSI_FUNCTION: str = "\x1b[1;36m"
 ANSI_UNRESOLVED: str = "\x1b[33m"
 ANSI_CYCLE: str = "\x1b[35m"
 INIT_MODULE_FILE_NAME: str = "__init__.py"

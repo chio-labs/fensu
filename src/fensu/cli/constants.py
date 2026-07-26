205a3c29ea744a3c821b7ea6fce270fee05c972d
Kevin Longe <kvlonge@gmail.com>
2026-07-26 09:15:23 +0100

    feat: add --color to check in both implementations

--- src/fensu/cli/constants.py
+++ src/fensu/cli/constants.py
@@ -3,3 +3,7 @@
 from __future__ import annotations
 
 SKILLS_METADATA_PROTOCOL_VERSION: int = 2
+COLOR_ALWAYS: str = "always"
+COLOR_AUTO: str = "auto"
+COLOR_NEVER: str = "never"
+NO_COLOR_ENVIRONMENT_VARIABLE: str = "NO_COLOR"
